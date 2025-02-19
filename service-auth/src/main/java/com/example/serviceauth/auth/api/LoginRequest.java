d673b60db8b9c0c13cb81928370f8cfdc759e245
burak-bulut <lotheas01@outlook.com>
2025-02-20 01:26:12 +0300

    security sıfırlandı. yeniden duzenlenecek.

--- service-auth/src/main/java/com/example/serviceauth/auth/api/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.serviceauth.auth.api;
-
-public record LoginRequest(
-        String username,
-        String password
-) {
-}
--- service-auth/src/main/java/com/example/serviceauth/auth/api/RefreshTokenRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.serviceauth.auth.api;
-
-public record RefreshTokenRequest(
-        String token
-) {
-}
